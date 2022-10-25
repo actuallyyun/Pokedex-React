60b57cab568d70833502b060019ecfc6aed61ee1
Yun Ji <this.jiyun@gmail.com>
2022-10-25 20:42:06 +0200

    Finish exercise A-F

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <Logo />
+      <h2>
+        <BestPokemon />
+      </h2></div>
+  )
+}
+
+function Logo() {
+  return (
+    <header>
+      <h1>Welcome to the Pokedex</h1>
+      <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt=""></img>
+    </header>
+  )
+
+}
+
+function BestPokemon() {
+  return <p>My favorite Pokemon is Pikachu.</p>
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,28 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const TitleComponent = () => {
+  return <h1>I am Title!</h1>
+}
+
+const Name = () => {
+  return <p>My Name is Ttitle</p>
+}
+
+const TtitleName = () => {
+  return (
+    <div>
+      <TitleComponent />
+      <Name />
+    </div>
+  )
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
